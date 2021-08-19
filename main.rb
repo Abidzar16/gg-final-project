a6242a20a02e831c66cb312e100489eb23949cb0
abidzarmgk <abidzarmgk@telkomuniversity.ac.id>
2021-08-19 17:53:02 +0700

    Minor changes on main controller

--- main.rb
+++ main.rb
@@ -1,15 +1,15 @@
 require "sinatra/base"
 require './app/controllers/user'
 require './app/controllers/post'
-# require './app/controllers/comment'
+require './app/controllers/comment'
 
 
 class MainApp < Sinatra::Base
   use UsersController
   use PostsController
-  # use CommentsController
+  use CommentsController
 
   get '/' do
-    "This is Main Controller!"
+    "This is Main App!"
   end
 end
