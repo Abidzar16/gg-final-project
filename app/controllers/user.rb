4c79367e3e37b0359fe34c4dfd0a0a1a2e560052
abidzarmgk <abidzarmgk@telkomuniversity.ac.id>
2021-08-24 22:48:19 +0700

    Minor Fix

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -17,8 +17,14 @@ class UsersController < Sinatra::Base
     user = User.new(params)
     response = user.save
 
+    @code = 201 # default value
+
+    if response.key? :error
+      @code = 400
+    end
+
     content_type :json
-    status 201
+    status @code
     body response.to_json
   end
 end
